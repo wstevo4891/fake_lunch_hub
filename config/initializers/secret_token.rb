0fdf6f6b0b0fac9d00bbf211da047207558f589d
Eric Stephenson <ericb.stephenson@gmail.com>
2016-02-04 19:35:34 -0800

    finally managed to create database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+FakeLunchHub::Application.config.secret_token = '70d851d83568e37b199001664e1a5b612b3c83f5bb3b2c9fb473b1ba78dab8babc4969459e69cc818de5f9664906fdac4219913e7fa6132dcb1426d46984abdd'
